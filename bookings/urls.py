8d7ff26d7d6b467b8c2e9b03cca9c164f6b959cd
ashur-k <ashurkanwal@yahoo.com>
2021-03-12 20:00:26 +0000

    started api list view to show room information

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('room_inforamation/', views.room_inforamation, name='room_inforamation'),
+    path('room_information_list/', views.room_information_list, name='room_information_list'),
 ]

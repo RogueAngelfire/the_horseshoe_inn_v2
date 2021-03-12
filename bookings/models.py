abb292ff97d08807a868c3ed1c8fd2ad277b1e8e
ashur-k <ashurkanwal@yahoo.com>
2021-03-12 18:47:59 +0000

    added methods to Roominformation models

--- bookings/models.py
+++ bookings/models.py
@@ -9,6 +9,10 @@ ROOM_TYPES = (
     )  
 
 class RoomInformation(models.Model):
+    
+    class Meta:
+        verbose_name_plural = 'RoomInformation'
+
     room_name = models.CharField(max_length=15, choices=ROOM_TYPES, default='None')
     room_image = models.ImageField(upload_to="media/rooms_images")
     room_description = models.CharField(max_length=255)
@@ -17,3 +21,10 @@ class RoomInformation(models.Model):
     room_price_per_night = models.FloatField()
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.room_name
+    
+    def delete(self, *args, **kwargs):
+        self.room_image.delete()
+        super().delete(*args, **kwargs)

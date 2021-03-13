1d37cd4d4af8be375b66ce636d809a8689e26e53
ashur-k <ashurkanwal@yahoo.com>
2021-03-13 15:24:29 +0000

    made Django and react to talk to each other

--- horseshoe_inn/settings.py
+++ horseshoe_inn/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'bookings.apps.BookingsConfig',
     # third party app
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +133,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CORS_ALLOW_ALL_ORIGINS = True
--- horseshoev2-app/src/App.js
+++ horseshoev2-app/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I have started Learnign React cool fgf
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+      this.state = {
+        roomList:[],
+        activeItem:{
+          id:null,
+          title:'',
+          completed:false,
+        },
+        editing:false,
+      }
+      this.fetchTasks = this.fetchTasks.bind(this)
+  };
+
+  componentWillMount(){
+    this.fetchTasks()
+  }
+
+  fetchTasks(){
+    console.log('Fetching...')
+    fetch('http://127.0.0.1:8000/api/room_information_list/')
+    .then(reponse => reponse.json())
+    .then(data=>
+      console.log('Data', data)
+      )
+  }
+
+  render(){
+
+    return(
+      <div className="container">
+        <div id="task-container">
+              <div  id="form-wrapper">
+                 <form id="form">
+                    <div className="flex-wrapper">
+                      <div style={{flex: 6}}>
+                          <input className="form-control" id="title" value="" type="text" name="title" placeholder="Add task.." />
+                        </div>
+                        <div style={{flex: 1}}>
+                          <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                        </div>
+                    </div>
+                </form>             
+              </div>
+            </div>  
+        <div id="list-wrapper">
+
+        </div>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;

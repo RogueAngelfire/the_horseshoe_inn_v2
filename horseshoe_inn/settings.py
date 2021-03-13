8128bfd81f7ba4a78bb8f70ed0521c79aeb85113
ashur-k <ashurkanwal@yahoo.com>
2021-03-13 22:49:13 +0000

    displaying data on web page

--- horseshoe_inn/settings.py
+++ horseshoe_inn/settings.py
@@ -134,4 +134,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
--- horseshoev2-app/src/App.js
+++ horseshoev2-app/src/App.js
@@ -21,34 +21,55 @@ class App extends React.Component{
   }
 
   fetchTasks(){
-    console.log('Fetching...')
     fetch('http://127.0.0.1:8000/api/room_information_list/')
     .then(reponse => reponse.json())
     .then(data=>
-      console.log('Data', data)
+      this.setState({
+        roomList:data
+      })
       )
   }
 
   render(){
-
+    var rooms = this.state.roomList
+    console.log("rooms",rooms)
     return(
       <div className="container">
         <div id="task-container">
-              <div  id="form-wrapper">
-                 <form id="form">
-                    <div className="flex-wrapper">
-                      <div style={{flex: 6}}>
-                          <input className="form-control" id="title" value="" type="text" name="title" placeholder="Add task.." />
-                        </div>
-                        <div style={{flex: 1}}>
-                          <input id="submit" className="btn btn-warning" type="submit" name="Add" />
-                        </div>
-                    </div>
-                </form>             
+          <div  id="form-wrapper">
+            <form id="form">
+              <div className="flex-wrapper">
+                <div style={{flex: 6}}>
+                    <input className="form-control" id="title" type="text" name="title" placeholder="Add task.." />
+                </div>
+                <div style={{flex: 1}}>
+                    <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                </div>
               </div>
-            </div>  
+            </form>             
+          </div>
+        </div>  
         <div id="list-wrapper">
-
+          {rooms.map(function(room, index){
+            return(
+              <div key={index} className="task-wrapper flex-wrapper">
+                <div style={{flex:7}}>
+                  <span>{room.room_name}: </span>
+                  <span>{room.room_description} </span>
+                  <span><img src={room.room_image}/></span><br></br>
+                  <span>{room.room_type}</span><br></br>
+                  <span>Customer Rating: {room.room_ratings}</span><br></br>
+                  <span>Price per Night: £{room.room_price_per_night}</span>
+                </div>
+                <div style={{flex:1}}>
+                  <button className="btn btn-sm btn-outline-info m-1">Edit</button>
+                </div>
+                <div style={{flex:1}}>
+                <button className="btn btn-sm btn-outline-dark m-1">Delete</button>
+                </div>
+              </div>
+            )
+          })}
         </div>
       </div>
     )
